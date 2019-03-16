dd54f2b2dee0e3e8d1546981fc10cbc68f1d3ac2
Arthur Busuttil <arthur.busuttil@epita.fr>
2019-03-16 18:31:15 +0100

    [DB][APP] Add billapp and intialization of the db

---
+++ yakabible/billapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class User(models.Model):
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    email = models.CharField(max_length=128)
+    pseudo = models.CharField(max_length=64)
+    password = models.BinaryField(max_length=20)
+    role = models.IntegerField()
+
+# Create your models here.
---
+++ yakabible/billapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def Index(request):
+    HttpResponse("OK")
+
+# Create your views here.
