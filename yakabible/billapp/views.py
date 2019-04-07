73c19c87a1d5b8d29fe3c0d35af476d79bac9b50
Thomas Lupin <thomas.lupin@epita.fr>
2019-04-07 18:11:19 +0200

    [CALENDAR] Fetch events from BDD

--- yakabible/billapp/views.py
+++ yakabible/billapp/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.views import generic
 from django.core.paginator import Paginator
 
-from .models import *
+from billapp.models import Event
 
 class IndexView(generic.TemplateView):
     template_name = "billapp/index.html"
@@ -16,5 +16,15 @@ def EventsJSON(request):
     start = request.GET.get('start')
     end = request.GET.get('end')
     # Fetch events from BDD
-    events = {}
-    return JsonResponse(events)
+    json = []
+    events = Event.objects.all().filter(end__gte=start, begin__lte=end)
+    for obj in events:
+        json.append({
+            'id': obj.pk,
+            'title': obj.title,
+            'start': obj.begin,
+            'end': obj.end,
+            'url': '/', # url of event/obj.pk
+        })
+
+    return JsonResponse(json, safe=False)
