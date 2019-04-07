254031a7eb925d717308b0a23b276bef422102c1
Thomas Lupin <thomas.lupin@epita.fr>
2019-04-07 18:26:51 +0200

    [ADMIN] Add all models to admin panel

--- yakabible/billapp/models.py
+++ yakabible/billapp/models.py
@@ -6,6 +6,8 @@ class Association(models.Model):
     logo_path = models.CharField(max_length=128)
     email = models.CharField(max_length=64)
     description = models.TextField()
+    def __str__(self):
+        return self.name
 
 class Event(models.Model):
     title = models.CharField(max_length=128)
@@ -18,22 +20,29 @@ class Event(models.Model):
     begin_register = models.DateTimeField()
     end_register = models.DateTimeField()
     place = models.CharField(max_length=128)
-    price_ionis = models.IntegerField()
-    price = models.IntegerField()
+    price_ionis = models.FloatField()
+    price = models.FloatField()
     ext_capacity = models.IntegerField()
     int_capacity = models.IntegerField()
     staff_capacity = models.IntegerField()
     promotion_image_path = models.CharField(max_length=128)
     validation_state = models.BooleanField()
+    def __str__(self):
+        return self.title
 
 class AssociationUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     association = models.ForeignKey(Association, on_delete=models.CASCADE)
     role = models.IntegerField()
+    def __str__(self):
+        return self.user.username + ' : ' + self.association.name + ' (' + str(self.role) + ')'
 
 class Ticket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     category = models.BooleanField()
     state = models.IntegerField()
+    def __str__(self):
+        return self.user.username + ' (' + self.event + ')'
+    
 # Create your models here.
