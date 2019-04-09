46b7a45354b092f7c0ae2a945d610a3e72ad90de
Arthur Busuttil <arthur.busuttil@epita.fr>
2019-04-09 13:36:15 +0200

    [EVENT][INSCRIPTION] Fix button and end_register display

--- yakabible/billapp/templatetags/tools.py
+++ yakabible/billapp/templatetags/tools.py
@@ -1,6 +1,9 @@
 from django import template
 from billapp.models import Ticket
+from datetime import datetime
+import pytz
 
+utc = pytz.UTC
 register = template.Library()
 
 @register.filter
@@ -10,3 +13,10 @@ def get_ext(event):
 @register.filter
 def get_int(event):
     return event.ticket_set.filter(user__email__iregex=r'.*epita.*').count()
+    
+@register.filter
+def in_the_bound(e):
+    begin = e.begin_register.replace(tzinfo=utc)
+    end = e.end_register.replace(tzinfo=utc)
+    now = datetime.now().replace(tzinfo=utc)
+    return now >= begin and now < end
