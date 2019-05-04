473e16eb060e55763a349bda6be4ae53e2ac535a
Thetoto <thomas.lupin@epita.fr>
2019-05-04 15:31:53 +0200

    [TESTS] Add first test on User

--- yakabible/billapp/tests.py
+++ yakabible/billapp/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
-
+from billapp.models import *
+from django.contrib.auth import authenticate, login, logout
+from billapp.insertions import *
+from billapp.forms import *
 # Create your tests here.
+
+class UserTestCase(TestCase):
+    def test_user_form(self):
+        form = Inscription_Form(data={
+            'email': "invalidemail.fr",
+            'firstname': 'toto',
+            'lastname': 'titi',
+            'pwd': 'password',
+            'pwd_conf': 'password',
+            'username': 'login'
+        })
+        self.assertEqual(form.is_valid(), False)
+        form = Inscription_Form(data={
+            'email': "valid@email.fr",
+            'firstname': 'toto',
+            'lastname': 'titi',
+            'pwd': 'password',
+            'pwd_conf': 'password',
+            'username': 'login'
+        })
+        self.assertEqual(form.is_valid(), True)
+        insert_user(form)
+
+    def test_connection_user(self):
+        self.test_user_form()
+        user = authenticate(username="login", password="password")
+        self.assertNotEqual(user, None)
