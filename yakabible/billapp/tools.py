a445601b291f2004b7786cbdd51469cec677bace
Thomas Lupin <thomas.lupin@epita.fr>
2019-06-03 22:55:16 +0200

    [HEROKU][USELESS] Remove useless import

--- yakabible/billapp/tools.py
+++ yakabible/billapp/tools.py
@@ -7,7 +7,6 @@ from django.conf import settings
 from PIL import Image
 
 from io import BytesIO, StringIO
-import dateutil.parser
 import datetime
 from django.http import HttpResponse
 from reportlab.pdfgen import canvas
